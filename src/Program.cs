3d7d6436a89c5af200b5935bc9ecf938de448e3b
mind0n <markyuanmj@126.com>
2020-10-05 22:24:00 +0800

    init check in

---
+++ src/Program.cs
@@ -0,0 +1,53 @@
+﻿using handy;
+using System;
+
+namespace yserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RouteManager rm = new RouteManager();
+            rm.Regist(typeof(WebBizUnit), true);
+            Extensions.Socket(8880, (x) =>
+            {
+                var r = x.Request;
+                var s = x.Response;
+                var txt = r.Raw;
+                log.i($"Request -> {txt}");
+                var ctx = new SocketHttpContext(x);
+                //var hs = new HttpStateMachine();
+                //hs.Parse(txt);
+                //var sr = hs.Result;
+                var content = rm.Execute(ctx);
+                if (content != null)
+                {
+                    s.Write(content);
+                    log.i($"Replied <- {content?.Length}");
+                }
+                else
+                {
+                    log.i("Replied internally");
+                }
+            });
+            Console.WriteLine("Press ESC to exit ...");
+            while (true)
+            {
+                var ch = Console.ReadKey();
+                if (ch.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+        }
+    }
+    public class WebBizUnit : BizUnit
+    {
+        public WebBizUnit(SocketHttpContext ctx) : base(ctx) { }
+        public string Default()
+        {
+            var file = "ws.html".SearchFile();
+            return "200 OK";
+        }
+    }
+}
