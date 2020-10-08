5a5afc2babcf221749f6e9638a4c6410c1cd3381
mind0n <markyuanmj@126.com>
2020-10-08 23:32:37 +0800

    Support customize root url & directory

--- src/Program.cs
+++ src/Program.cs
@@ -58,12 +58,37 @@ namespace yserver
     }
     public class WebBizUnit : BizUnit
     {
+        static EmrInstance cfg => Program.cfg;
         public WebBizUnit(SocketHttpContext ctx) : base(ctx) { }
         public string Default()
         {
-            //var file = Program.cfg
-            var r = string.Join("\\",Request.Routes);
-            return $"{r} - {Environment.CurrentDirectory}";
+            var rooturl = cfg.Get("rooturl", "/");
+            var rootdir = cfg.Get("rootdir", Environment.CurrentDirectory);
+            rootdir = rootdir.FullPath(Environment.CurrentDirectory);
+            var r = string.Join("/",Request.Routes);
+            if (r.StartsWith(rooturl))
+            {
+                r = r.Replace(rooturl, string.Empty);
+            }
+            else if (!"/".Equals(rooturl))
+            {
+                Response.HttpStatusCode = 404;
+                return $"404 - not found - {r}";
+            }
+            if (!r.StartsWith('/'))
+            {
+                r = '/' + r;
+            }
+            var p = $"{rootdir}{r.Replace('/', '\\')}".Replace("..", string.Empty);
+            if (File.Exists(p))
+            {
+                Response.HttpStatusCode = 200;
+                var type = Response.Socket.WriteFile(p);
+                Response.ContentType = type;
+                return null;
+            }
+            Response.HttpStatusCode = 404;
+            return $"404 not found - {p}";
         }
     }
 }
